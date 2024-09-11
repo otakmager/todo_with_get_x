60b720d9ed7ec59dc5f9f86d141db58cbb9a3631
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2024-09-11 08:10:38 +0700

    create todo model

--- lib/app/data/model/todo.dart
+++ lib/app/data/model/todo.dart
@@ -1,1 +1,39 @@
+class ToDoData {
+  int code;
+  String message;
+  List<ToDo?> items;
 
+  ToDoData({
+    required this.code,
+    required this.message,
+    required this.items,
+  });
+
+  ToDoData.fromJson(Map<String, dynamic> json)
+      : code = json['code'] as int? ?? -999,
+        message = json['message'] ?? 'Uknown error',
+        items = (json['items'] as List<dynamic>?)
+                ?.map((e) => ToDo.fromJson(e as Map<dynamic, String>))
+                .toList() ??
+            [];
+}
+
+class ToDo {
+  String id;
+  String title;
+  String description;
+  bool isCompleted;
+
+  ToDo({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.isCompleted,
+  });
+
+  ToDo.fromJson(Map<dynamic, String> json)
+      : id = json['id'] ?? '',
+        title = json['title'] ?? '',
+        description = json['description'] ?? '',
+        isCompleted = json['isCompleted'] as bool? ?? false;
+}
